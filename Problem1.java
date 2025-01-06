c4888337ad72dbb68bae39e0dfa84da832856d42
Abhinav Gangadharan <144839406+abhinavg-neu@users.noreply.github.com>
2025-01-05 20:46:40 -0500

    attempt at Competitive Coding-1

--- Problem1.java
+++ Problem1.java
@@ -1,1 +1,45 @@
 
+// Find Missing Number in a sorted array of size n with one missing number
+// Time Complexity: O(log n)
+// Space Complexity: O(1)
+// Did this code successfully run on Leetcode : Yes
+// thought process: at every step we're checking which part is sorted and then we're checking if the target lies in that part
+// if it does, we're doing a binary search in that part, if it doesn't, we're doing a binary search in the other part
+// we're doing this until we find the target or the low and high pointers cross each others
+public class Problem1 {
+public int findMissingNumber (int[] nums, int low , int high){
+    if (nums.length == 0 || nums == null){
+        return -1;
+    }
+    if (nums[0] != 1){
+        return 1;
+    }
+    if (nums[high] != high +2){
+        return high +2;
+    }
+    int mid =0;
+    while (low <=high){
+         mid =low + (high - low)/2;
+        if (nums[mid] != mid+1 && nums[mid -1] == mid){
+            return mid +1;
+        }
+        if (nums[mid] == mid +1){
+            low = mid +1;
+        } else {
+            high = mid-1;
+        }
+
+    }
+    return mid +1;
+}
+public static void main(String[] args){
+    Problem1 problem1 = new Problem1();
+    // int[] nums = {1,2,3,4,5,6,7,8};
+    // int[] nums = {2,3,4,5,6,7,8,9};
+    int[] nums = {1,2,4,5,6,7,8,9};
+    // int[] nums = {1,3,4,5,6,7,8,9};
+    // int[] nums = {1,2,3,4,5,7,8,9};
+
+    System.out.println(problem1.findMissingNumber(nums, 0, nums.length-1));
+}
+}
--- Problem2.java
+++ Problem2.java
@@ -1,1 +1,69 @@
 
+// Problem 2: Design Min Heap
+public class Problem2 {
+
+    int heap[];
+    int size;
+    public Problem2 (int maxSize){
+        heap = new int[maxSize];
+        size = 0;
+    }
+
+    //time complexity: O(logn)
+    public void add (int i){
+        if (size == heap.length){
+            throw new IllegalStateException("Heap is full");
+        }
+
+        heap[++size] = i;
+        heapify();
+
+    }
+    //time complexity: O(logn)
+    public void remove (){
+        heap[1] = heap[size --];
+        heapify();
+    }
+    //time complexity: O(1)
+    public int getMin(){
+        if (size == 0){
+            throw new IllegalStateException("Heap is empty");
+        }
+        return heap[1];
+    }
+
+    //time complexity: O(1)
+    public int getParent (int i){
+        return heap[i/2];
+    }
+    //time complexity: O(1)
+    public int getLeftChild (int i){
+        return heap[2*i];
+    }
+
+    public int getRightChild(int i){
+        return heap[2*i+1];
+
+    }
+    public void swap (int leftPos, int rightPos){
+        int temp = heap[leftPos];
+        heap[leftPos] =  heap[rightPos];
+        heap[rightPos] = temp;
+    }
+    public void heapify (){
+       int pos = size;
+       while (pos >1){
+        if (heap[pos] < getParent(pos) ){
+            swap(pos, pos/2);
+        }
+       pos--;
+       } 
+    }
+    public static void main(String[] args) {
+        Problem2 minHeap = new Problem2(10);
+        minHeap.add(15);
+        // minHeap.add(10);
+        // minHeap.add(20);
+        System.out.println(minHeap.getMin());
+    }
+}
